f194876f19cfa1c6ebcc7fe945aaadc456cc6ec9
Marius Unsel <munsel@munsel.de>
2020-07-25 19:37:24 +0200

    use TTGO board

--- CoronaAppDetector.ino
+++ CoronaAppDetector.ino
@@ -5,14 +5,38 @@
 #include <BLEAddress.h>
 #include <Ticker.h>
 #include <unordered_set>
+//Libraries for OLED Display
+ 
+#include <TFT_eSPI.h>
+#include <SPI.h>
 
-#define SEGMENT_A 25
-#define SEGMENT_B 26
-#define SEGMENT_C 32
-#define SEGMENT_D 33
-#define SEGMENT_E 12
-#define SEGMENT_F 13
-#define SEGMENT_G 27
+#ifndef TFT_DISPOFF
+#define TFT_DISPOFF 0x28
+#endif
+
+#ifndef TFT_SLPIN
+#define TFT_SLPIN   0x10
+#endif
+
+#define TFT_MOSI            19
+#define TFT_SCLK            18
+#define TFT_CS              5
+#define TFT_DC              16
+#define TFT_RST             23
+
+#define TFT_BL              4   // Display backlight control pin
+#define ADC_EN              14  //ADC_EN is the ADC detection enable port
+#define ADC_PIN             34
+#define BUTTON_1            35
+#define BUTTON_2            0
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+
+
+// Module connection pins (Digital Pins)
+#define CLK 27
+#define DIO 26
+
  
 
 #define SLOTS 3
@@ -23,122 +47,26 @@ String Address = "00:AA:FF:13:37:42";
 Ticker Tic;
 static BLEAddress *pServerAddress;
 BLEScan* pBLEScan ;
-int scanTime = 30; 
+int scanTime = 10; 
 
-
+unsigned char num = 0;
 
 
  void printNumber(int number)
  {
-  /*pls dont juge me*/   
-  if(number == 0){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 1);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 0);
-  }
-    if(number == 1){
-    digitalWrite(SEGMENT_A, 0);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 0);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 0);
-    digitalWrite(SEGMENT_G, 0);
-  }
-    if(number == 2){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 0);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 1);
-    digitalWrite(SEGMENT_F, 0);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number == 3){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 0);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number == 4){
-    digitalWrite(SEGMENT_A, 0);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 0);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number == 5){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 0);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number == 6){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 0);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 1);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number == 7){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 0);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 0);
-    digitalWrite(SEGMENT_G, 0);
-  }
-    if(number == 8){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 1);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 1);
-  }   
-  if(number == 9){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 1);
-    digitalWrite(SEGMENT_C, 1);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 1);
-    digitalWrite(SEGMENT_G, 1);
-  }
-    if(number > 9){
-    digitalWrite(SEGMENT_A, 1);
-    digitalWrite(SEGMENT_B, 0);
-    digitalWrite(SEGMENT_C, 0);
-    digitalWrite(SEGMENT_D, 1);
-    digitalWrite(SEGMENT_E, 0);
-    digitalWrite(SEGMENT_F, 0);
-    digitalWrite(SEGMENT_G, 1);
-  }
-  
-  
+  String out = String(number);
+	tft.fillScreen(TFT_BLACK);
+	tft.drawString("Corona-App", 5, 5);
+	tft.drawString("count: "+out, 5, 16*3*2);
+	//tft.drawString(out, 150, 16*3*2);
+	Serial.println("PRINT NUMBER");
  }
 
 
 int slot = 0;
 int old = 0;
 std::unordered_set<std::string>* seen[SLOTS];
-void sTick()  // Wird jede Sekunde ausgefüert
+void sTick()  // gets calles every second
 { 
   slot = ++slot%SLOTS; //increment in %10
   seen[slot]->clear();//clear
@@ -166,23 +94,22 @@ void sTick()  // Wird jede Sekunde ausgefüert
   if (fullSlots > 0)
     near = sum / fullSlots;
   
-  //tell serial the number of devices
+  // tell serial the number of devices
   if (near != old){
     Serial.println(near);
     old = near;
   }
-  //print to 7segment display. 
   printNumber(near);
 }
  
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks
 {
-    void onResult(BLEAdvertisedDevice advertisedDevice) //is called when a ble beacon is found
+    void onResult(BLEAdvertisedDevice advertisedDevice) // is called when a ble beacon is found
     {
-      BLEUUID coronaUUID("FD6F"); //from https://covid19-static.cdn-apple.com/applications/covid19/current/static/contact-tracing/pdf/ExposureNotification-BluetoothSpecificationv1.2.pdf
-      if (advertisedDevice.isAdvertisingService(coronaUUID)) //if this is a corona beacon
+      BLEUUID coronaUUID("FD6F"); // from https://covid19-static.cdn-apple.com/applications/covid19/current/static/contact-tracing/pdf/ExposureNotification-BluetoothSpecificationv1.2.pdf
+      if (advertisedDevice.isAdvertisingService(coronaUUID)) // corona beacon found?
       {
-        seen[slot]->insert(advertisedDevice.getAddress().toString()); //save sender mac to the set
+        seen[slot]->insert(advertisedDevice.getAddress().toString()); // save sender mac to the set
       }
       else if (advertisedDevice.haveServiceUUID())
       {
@@ -207,21 +134,39 @@ void setup()
   }
   Serial.begin(115200);
   Serial.println("");
-  Serial.println("Starte BLE Scanner");
+  Serial.println("Start BLE Scanner");
   pServerAddress = new BLEAddress(Address.c_str());
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pinMode(SEGMENT_A, OUTPUT);
-  pinMode(SEGMENT_B, OUTPUT);
-  pinMode(SEGMENT_C, OUTPUT);
-  pinMode(SEGMENT_D, OUTPUT);
-  pinMode(SEGMENT_E, OUTPUT);
-  pinMode(SEGMENT_F, OUTPUT);
-  pinMode(SEGMENT_G, OUTPUT);
 
-  //every TICK_INTERVAL second we want to do some stuff
+
+  // every TICK_INTERVAL second we want to do some stuff
   Tic.attach( TICK_INTERVAL,sTick);
+ 
+	tft.init();
+
+	tft.setTextSize(3);
+
+	if (TFT_BL > 0) {                           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+		pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
+		digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+	}
+
+
+    tft.setRotation(1);
+    tft.fillScreen(TFT_RED);
+    delay(20);
+    tft.fillScreen(TFT_BLUE);
+    delay(20);
+    tft.fillScreen(TFT_GREEN);
+    delay(20);
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextDatum(MC_DATUM);
+   
+    tft.drawString("init...", tft.width() / 2, tft.height() / 2);
+   
 }
  
 void loop()
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # CoronaAppDetector
 
-Some code and sample hardware setup to show how many corona apps are in proximity.
-Uses esp32 and some 7 segment display
+Some code to show how many corona apps are in proximity.
+It uses the TTGO esp32 module with integrated RGB TFT display. 
 
-![My Hardware](/hardware.jpg)
+## Display Library Configuration
+
+To config the [TFT eSPI library](https://github.com/Bodmer/TFT_eSPI), you have to edit `User_Setup_Select.h` in the library location.
+Comment out `#include <User_Setup.h>` and uncomment `#include <User_Setups/Setup25_TTGO_T_Display.h>`.
+
+![hardware picture](/hardware.jpg)
